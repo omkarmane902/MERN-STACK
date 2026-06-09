7bd103619030a9be5696b5ab559d3334c83ec79c
onkar Mane <157266293+omkarmane902@users.noreply.github.com>
2026-06-09 06:40:40 +0100

    Add files via upload

---
+++ JavaScript/Tokens/separators and punctuators/punctuators.js
@@ -0,0 +1,12 @@
+//? In JavaScript, separators and punctuators (punctuation symbols) are special characters used to separate code elements and define the structure of a program.
+
+
+// Common Separators / Punctuators
+// Symbol	Purpose	Example
+// ;	Ends a statement	let x = 10;
+// ,	Separates items	let a = 1, b = 2;
+// ()	Groups expressions, function parameters	sum(a, b)
+// {}	Defines a block of code	if (x > 0) { ... }
+// []	Array literals or indexing	arr[0]
+// .	Accesses object properties	person.name
+// :	Separates key and value in objects	{name: "John"}
