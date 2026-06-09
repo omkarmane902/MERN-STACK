e2911eef403b2f5cd6b539e1624100687c791d2b
onkar Mane <157266293+omkarmane902@users.noreply.github.com>
2026-06-09 05:24:43 +0100

    Add files via upload

---
+++ Features of Javascript/Features_of_Javascript.js
@@ -0,0 +1,20 @@
+//! 1. Scripting & High-Level
+// What it means: It uses easy human words. You don't build a computer from scratch; you just write quick rules for a browser.
+// Real-life example: It's like writing a sticky note on a washing machine that says: "If the clothes are wet, spin for 10 minutes."
+
+//! 2. Interpreted
+// What it means: The computer reads your code line-by-line and acts on it instantly. It does not look ahead.
+// Real-life example: It’s like cooking a dish while reading the recipe line-by-line. You read Line 1, do it, then read Line 2.
+
+//! 3. Single-Threaded & Synchronous
+// What it means: JavaScript has only one worker who does things one at a time, in order. It cannot do two things at the exact same split second.
+// Real-life example: A single person checking out at a grocery counter. The cashier must finish scanning the first person's items completely before they can even look at the next person's food.
+
+//! 4. Object-Oriented & Object-Based
+// What it means: It loves grouping related things together into small, neat "packets" called Objects.
+// Real-life example: Instead of having loose details everywhere like wheel, steeringWheel, and engine, you pack them all inside one single folder labeled Car.
+
+//! 5. Loosely & Dynamically Typed
+// What it means: You do not have to label your storage boxes. JavaScript doesn't care what you put inside; it figures it out automatically and lets you swap items whenever you want.
+
+
