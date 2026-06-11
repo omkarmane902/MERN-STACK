4a492e9255e2fd8c89fdfd021e82e024cc410ceb
onkar Mane <157266293+omkarmane902@users.noreply.github.com>
2026-06-11 03:58:31 +0100

    Add files via upload

---
+++ JavaScript/Tokens/Literals/Non-primitive/Functions/Notes.js
@@ -0,0 +1,17 @@
+// It is set of instruction it is used to perfom spefice task.
+// it is reusable block of code.
+//we must call the function for execution.
+//there are multiple type of fuction.
+//it is an ES6 feature introduced to overcome some of the issues with legacy functions.
+//It cannot be called before declaration
+//it make the syntax more easier.
+//does not have it own this keyword context
+
+//! type of function
+// 1.arrow function 
+// 2.function declaration
+// 3.function as expression
+// 4. Immediately Invoked Function Expression (IIFE)
+// 5.Anonymous Function
+// 6.Higher-Order Function
+// 7.Callback Function
