1e31e2a30480227b116a72e9a439fe1a57590741
onkar Mane <157266293+omkarmane902@users.noreply.github.com>
2026-06-11 05:33:53 +0100

    Add files via upload

---
+++ JavaScript/Tokens/Literals/Non-primitive/Functions/Function declaration/demo.js
@@ -0,0 +1,144 @@
+//Function declaration is a legacy code function which has some drowback that way it is not recommented to use.
+
+
+// Example
+
+//!Task 1: Greeting Function
+// Create a function that prints a greeting message.
+//? Expected Output:
+// Hello, Om!
+
+// gree("omakr")
+
+
+// function gree(data){
+
+//     console.log(`hello ${data}`);
+    
+// }
+
+
+//! Task 2: Add Two Numbers
+// Create a function that accepts two numbers and returns their sum.
+//? Example:
+// add(10, 20);
+
+// add(10,20)
+// function add(num1 , num2) {
+//     console.log(`Add Two Numbers is ${num1 + num2}`);
+// }
+
+//! Task 3: Check Even or Odd
+// Create a function that checks whether a number is even or odd.
+//? Example:
+// checkEvenOdd(7);
+
+// checkEvenOdd(15)
+
+// function checkEvenOdd(num) {
+    
+//     let check = (num%2===0) ? `This is even number ${num}`:`This is odd number ${num}`
+
+//     console.log(check);
+    
+// }
+
+
+
+
+//! Task 5: Calculate Square
+//Create a function that returns the square of a number.
+//todo  Example:
+// square(5);
+//? Output:
+// 25
+
+
+// calculate(5)
+
+// function calculate(num)
+// {
+//     let square = num * num
+
+//     console.log(square);
+    
+// }
+
+
+//! Task 6: Check Voting Eligibility
+// Create a function that accepts age and returns:
+// "Eligible" if age is 18 or above
+// "Not Eligible" otherwise
+//todo Example:
+// checkVotingEligibility(20);
+//? Output:
+// Eligible
+
+
+// checkVotingEligibility(20)
+
+// function checkVotingEligibility(age) {
+//     console.log(age>=18 ? 'Eligible': 'Not Eligible' );
+    
+// }
+
+
+
+//! Task 7: Reverse a String
+// Create a function that reverses a string.
+//todo  Example:
+// reverseString("Omkar");
+//? Output:
+// rakmO
+
+// reverseString("Omkar")
+
+// function reverseString(str) {
+    
+//     let revstr = str.split("").reverse().join("")
+
+//     console.log(revstr);
+    
+// }
+
+
+
+//! Task 8: Count Characters
+// Create a function that returns the length of a string.
+//todo Example:
+// countCharacters("JavaScript");
+//? Output:
+// 10
+
+// countCharacters("JavaScript")
+
+// function countCharacters(str) {
+    
+//     let count = str.length
+//     console.log(count);
+    
+// }
+
+
+//! Task 9: Check Palindrome
+// A palindrome reads the same forward and backward.
+//todo Example:
+// isPalindrome("madam");
+//? Output:
+// true
+
+
+// isPalindrome("madam");
+
+// function isPalindrome(str) {
+    
+//     let revstr = str.split("").reverse().join("")
+//     if (str===revstr) {
+//         console.log(true);
+//     } else {
+//         console.log(false);
+        
+//     }
+// }
+
+
