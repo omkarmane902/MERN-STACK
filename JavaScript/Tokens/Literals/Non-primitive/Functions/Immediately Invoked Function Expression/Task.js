de34a8f9068134a4abee594d0233f80511dbed78
onkar Mane <157266293+omkarmane902@users.noreply.github.com>
2026-06-12 05:47:11 +0100

    Add files via upload

---
+++ JavaScript/Tokens/Literals/Non-primitive/Functions/Immediately Invoked Function Expression/Task.js
@@ -0,0 +1,42 @@
+//! An IIFE is a function that runs immediately after it is created.
+
+
+
+//! Task 1: Welcome Message
+// Create an IIFE that prints:
+// Welcome to JavaScript!
+
+
+// (function() {
+//     console.log("Welcome to JavaScript!");
+    
+// })()
+
+
+
+
+//! Task 2: Calculate Sum
+// Create an IIFE that adds 10 and 20 and prints the result.
+
+
+// ((num1, num2)=>{
+//   console.log(num1 + num2); 
+   
+// })(10 ,20)
+
+
+
+
+
+//! Task 3: Find Largest Number
+// Create an IIFE that compares 50 and 80 and prints the larger number.
+// Output:
+// 80
+
+((num1,num2)=>{
+    console.log(Math.max(num1,num2));
+    
+})(50, 80)
+
+
+
