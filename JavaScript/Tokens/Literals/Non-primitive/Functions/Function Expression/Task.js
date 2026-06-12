8500fcdd4d93c239000a4f89d673cf93e1c490a6
onkar Mane <157266293+omkarmane902@users.noreply.github.com>
2026-06-12 05:12:00 +0100

    Add files via upload

---
+++ JavaScript/Tokens/Literals/Non-primitive/Functions/Function Expression/Task.js
@@ -0,0 +1,38 @@
+//! Example 1: Greeting Function
+
+
+const greet = function(name) {
+    console.log(`Hello, ${name}!`);
+};
+
+greet("Om");
+
+// Output:
+// Hello, Om!
+
+
+//! Example 2: Add Two Numbers
+
+const add = function(a, b) {
+    return a + b;
+};
+
+console.log(add(10, 20));
+
+// Output:
+// 30
+
+
+
+//! Example 2: Add Two Numbers
+
+let squre = function(num) {
+    
+    return num*num;
+
+}
+
+console.log(squre(10));
+
+// Output:
+// 100
