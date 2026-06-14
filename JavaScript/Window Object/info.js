d44745939dba9bc077fbe7a8651f299c2733ac68
onkar Mane <157266293+omkarmane902@users.noreply.github.com>
2026-06-14 16:34:56 +0100

    Add files via upload

---
+++ JavaScript/Window Object/info.js
@@ -0,0 +1,15 @@
+// Window Object
+// It is the parent object of all objects in JavaScript (browser environment).
+// It is available in the heap area.
+// Some built-in objects and functions inside the window object are:
+// console
+// alert()
+// prompt()
+// document (DOM)
+// All properties and methods of the window object can be accessed directly without using the window. prefix.
+// Example:
+// JavaScript
+// alert("Hello");
+// window.alert("Hello");
+// Both are valid.
+
