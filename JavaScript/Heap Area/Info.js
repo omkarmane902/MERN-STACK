1825c830adbb1a04501167856058a4490fbf32a9
onkar Mane <157266293+omkarmane902@users.noreply.github.com>
2026-06-14 16:33:36 +0100

    Add files via upload

---
+++ JavaScript/Heap Area/Info.js
@@ -0,0 +1,9 @@
+//! Heap Area
+
+// It is a memory allocation area in the JavaScript engine.
+// It stores:
+// All objects and functions.
+// Non-primitive data types.
+// Variables declared using var (objects/references are stored in heap).
+// Reference values are stored in the heap area.
+
