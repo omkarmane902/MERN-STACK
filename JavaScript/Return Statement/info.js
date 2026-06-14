3558b3684923288fe82e50ad27aebc2e9fb74275
onkar Mane <157266293+omkarmane902@users.noreply.github.com>
2026-06-14 16:37:07 +0100

    Add files via upload

---
+++ JavaScript/Return Statement/info.js
@@ -0,0 +1,14 @@
+// Return Statement
+// A return statement is a control transfer statement.
+// It stops the execution of the current function and returns a value.
+// It can be used inside:
+// Functions
+// Conditional blocks
+// Loops (inside functions)
+// Example:
+// JavaScript
+// function add(a, b) {
+//   return a + b;
+// }
+
+// console.log(add(10, 20)); // 30
