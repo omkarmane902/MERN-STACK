5f2c44d1f99623195f442fd15b13e7defcc315ec
onkar Mane <157266293+omkarmane902@users.noreply.github.com>
2026-06-14 16:34:17 +0100

    Add files via upload

---
+++ JavaScript/Call Stack/info.js
@@ -0,0 +1,8 @@
+// Call Stack
+// The call stack is responsible for executing JavaScript code.
+// It follows the LIFO (Last In, First Out) principle.
+// Important Points
+// The call stack has an execution context for the global scope called the Global Execution Context.
+// On top of the global execution context, a new execution context is created whenever a function is called.
+// The last called context is destroyed first.
+// Memory allocation for local variables and primitive data types happens in the call stack.
