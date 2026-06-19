cb919848fef33e9e0623f5e61fd3cc1a1d16815f
onkar Mane <157266293+omkarmane902@users.noreply.github.com>
2026-06-19 19:38:19 +0100

    Add files via upload

---
+++ JavaScript/CURD/Task.js
@@ -0,0 +1,36 @@
+//! Create 
+
+const person = {
+  name: "Omkar",
+  age: 22,
+  city: "Pune"
+};
+
+
+//! Read
+
+console.log(person);
+
+
+//! Update
+
+person.city = "Yadrav"
+
+console.log(person);
+
+
+
+//! Insert 
+
+person.address = "mane mala"
+
+
+console.log(person);
+
+
+delete person.address 
+
+console.log(person);
+
+
+
