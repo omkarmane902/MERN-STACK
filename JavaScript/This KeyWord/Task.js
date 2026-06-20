33b565d89e006e66606e414391e86ec200165562
onkar Mane <157266293+omkarmane902@users.noreply.github.com>
2026-06-20 19:35:56 +0100

    Add files via upload

---
+++ JavaScript/This KeyWord/Task.js
@@ -0,0 +1,42 @@
+let data = {
+    name:"omkar",
+    salary:7000,
+
+    fun () {
+         console.log(`This side ${this.name} and salary is ${this.salary}`);
+         
+     }
+}
+
+data.fun()
+
+
+
+let info ={
+    surname : mane,
+    brand: sarkar,
+
+     fun (){
+        console.log(this);
+        
+     }
+}
+
+info.fun()
+
+
+
+console.log(this);
+
+
+
+let student = {
+    name : bapu,
+    salary:8000000,
+
+    arroyfun = ()=>{
+
+        console.log(this);
+        
+    }
+}
