732377511f2289eef07095cf86aef71a02f0e42a
onkar Mane <157266293+omkarmane902@users.noreply.github.com>
2026-06-30 10:07:39 +0530

    Add files via upload

---
+++ JavaScript/Event/Task1/script.js
@@ -0,0 +1,19 @@
+let btn = document.querySelector('.btn');
+let text = document.querySelector('.text')
+
+btn.addEventListener('click',function(){
+    text.classList.toggle('text')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
